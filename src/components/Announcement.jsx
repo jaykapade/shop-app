73e410bf4a4d10a438e045f84cb7391ed83454ba
jaykapade <jaykapade@gmail.com>
2021-10-12 22:03:23 +0530

    Added NavBar and Announcement Components

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  height: 3rem;
+  background-color: teal;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.4rem;
+  font-size: 500;
+`
+
+const Announcement = () => {
+  return (
+    <Container>
+      Superb Deals! Amazing Offers! Free Shipping on Orders over $100
+    </Container>
+  )
+}
+
+export default Announcement
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,83 @@
+import { Badge } from '@material-ui/core'
+import { grey } from '@material-ui/core/colors'
+import { Search, ShoppingCartOutlined } from '@material-ui/icons'
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  height: 10vh;
+`
+const Wrapper = styled.div`
+  padding: 1rem 2rem;
+
+  display: flex;
+  justify-content: space-between;
+`
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`
+const Language = styled.span`
+  font-size: 1.4rem;
+  cursor: pointer;
+`
+const SearchContainer = styled.div`
+  border: 0.1rem solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 2.5rem;
+  padding: 0.5rem;
+`
+const Input = styled.input`
+  border: none;
+`
+
+const Center = styled.div`
+  flex: 1;
+`
+const Logo = styled.h1`
+  font-weight: bold;
+  text-align: center;
+`
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+`
+const MenuItem = styled.div`
+  font-size: 1.4rem;
+  cursor: pointer;
+`
+
+// const Left = styled.div``
+
+const NavBar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input /> <Search style={{ color: grey }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>GUCCI</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={0} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default NavBar
