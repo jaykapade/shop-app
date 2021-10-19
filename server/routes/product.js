c91aa10a2a65cb901eede1983210809b78768d43
jaykapade <jaykapade@gmail.com>
2021-10-19 22:54:32 +0530

    Added user and some product routes with functionality Modified fields in Product Model

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,3 +1,57 @@
+const Product = require("../models/Product");
+const {
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
+
 const router = require("express").Router();
+
+//* Create Product: Admin
+router.post("/", verifyTokenAndAdmin, async (req, res) => {
+  const newProduct = new Product(req.body);
+  try {
+    const savedProduct = await newProduct.save();
+    res.status(200).json(savedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//* Update Product: Admin
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//* Delete Product: Admin
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json("Product has been deleted");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//* Get Specific Product
+router.get("/find/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    res.status(200).json({ product });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- server/routes/user.js
+++ server/routes/user.js
@@ -38,12 +38,50 @@ router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
   }
 });
 
-//* Get AdminUser
+//* Get Specific User: Admin
 router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
     const { password, ...others } = user._doc;
     res.status(200).json({ ...others });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//* Get All Users : limit set to 5 : Admin
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new;
+  try {
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
+    res.status(200).json({ users });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//* Get User Stats: Admin
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastyear = new Date(date.setFullYear(date.getFullYear() - 1));
+  try {
+    const data = await User.aggregate([
+      { $match: { createdAt: { $gte: lastyear } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json({ data });
   } catch (err) {
     res.status(500).json(err);
   }
