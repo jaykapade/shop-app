ec75aebe7ac6b6375884e98be98f2b953334d0d4
jaykapade <jaykapade@gmail.com>
2021-10-20 16:46:01 +0530

    Fixed timestamp issue in Product Model and Added product routes

--- server/routes/product.js
+++ server/routes/product.js
@@ -54,4 +54,30 @@ router.get("/find/:id", async (req, res) => {
   }
 });
 
+//* Get All Products : limit set to 5
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  const qNew = req.query.new;
+  const qCategory = req.query.category;
+  try {
+    let products;
+    console.log(qNew);
+    console.log(qCategory);
+    if (qNew && qCategory === false) {
+      products = await Product.find().sort({ createdAt: -1 }).limit(5);
+    } else if (qCategory && qNew === false) {
+      products = await Product.find({ categories: { $in: [qCategory] } });
+    } else if (qNew && qCategory) {
+      products = await Product.find({ categories: { $in: [qCategory] } }).sort({
+        createdAt: -1,
+      });
+    } else {
+      products = await Product.find();
+    }
+
+    res.status(200).json({ products });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
