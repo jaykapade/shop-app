258275b236d4374bddc147adbc0d84bb19366de2
jaykapade <jaykapade@gmail.com>
2021-10-27 23:51:03 +0530

    test commit

--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 //! Add Secret Key Directly here instead of PROCESS.env.STRIPE_KEY
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+const stripe = require("stripe")(
+  "sk_test_51JJaIxSHq5aXuMQIcU7bMdExfiJjBuayYnbpDnarUXyDbOP5bNbUcUk7Ecx3sGMT64Y0pTHyfckUwMQS2slyR6aF00ZosidL1k"
+);
 
 router.post("/payment", (req, res) => {
   // console.log(process.env.STRIPE_KEY);
