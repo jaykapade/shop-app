01e56b6df87ed71008f880b9e8c425c2f9b41839
jaykapade <jaykapade@gmail.com>
2021-10-31 17:09:34 +0530

    Added FeatureInfo and Order

--- server/routes/order.js
+++ server/routes/order.js
@@ -68,7 +68,7 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
 //* Get Monthly Income: Admin
 router.get("/income", verifyTokenAndAdmin, async (req, res) => {
   const date = new Date();
-  const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+  const lastMonth = new Date(date.setMonth(date.getMonth() - 2));
   const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
 
   try {
